c884cd0aa86d37fd5ebb0f5fcdbfb213c394282e
3xcc <husseinshamin@gmail.com>
2025-09-01 08:25:51 +0000

    Add Ticket model and trim dependencies

--- app/models/ticket.py
+++ app/models/ticket.py
@@ -1,6 +1,14 @@
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
+from typing import Optional
 
-class TicketCreate(BaseModel):
+class Ticket(SQLModel, table=True):
+    ticket_id: str = Field(primary_key=True)
+    name: str
+    id_card_number: str
+    date_of_birth: str
+    phone_number: str
+
+class TicketCreate(SQLModel):
     name: str
     id_card_number: str
     date_of_birth: str
