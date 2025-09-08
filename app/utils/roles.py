09f3d4855e9efb9020cf78a1cf688c5dbc0f0f9a
3xcc <husseinshamin@gmail.com>
2025-09-08 18:37:44 +0000

    Pin bcrypt to 4.0.1 to fix passlib __about__ warning

--- app/utils/roles.py
+++ app/utils/roles.py
@@ -1,7 +1,7 @@
 ROLE_PERMISSIONS = {
-    "admin": ["create_user", "edit_ticket", "delete_ticket", "export", "verify_payment"],
-    "subadmin": ["edit_ticket", "delete_ticket", "export"],
-    "editor": ["edit_ticket"],
+    "admin": ["scan_ticket", "create_user", "edit_ticket", "delete_ticket", "export", "verify_payment"],
+    "subadmin": ["scan_ticket", "edit_ticket", "delete_ticket", "export"],
+    "editor": ["scan_ticket", "edit_ticket"],
     "scanner": ["scan_ticket"]
 }
 
